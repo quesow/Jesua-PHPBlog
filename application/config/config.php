8cfda389518855064b4bbbbffced67e2ae93371c
JESUA BETANCOR ALEMAN <jbetancor.0011@gmail.com>
2016-08-01 18:32:12 +0100

    Config production URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'https://cryptic-mountain-50096.herokuapp.com/';
-$config['base_url'] = 'http://localhost/jesua-PHPBlog/';
+$config['base_url'] = 'https://cryptic-mountain-50096.herokuapp.com/';
+//$config['base_url'] = 'http://localhost/jesua-PHPBlog/';
 
 /*
 |--------------------------------------------------------------------------
